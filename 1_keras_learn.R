7b96dd79fb148dbf2fa10771921f60baa2c39e64
rexfelix <rexfelix.vonspica@gmail.com>
2020-10-04 06:37:43 +0900

    first keras model trained on R

---
+++ 1_keras_learn.R
@@ -0,0 +1,33 @@
+library(keras)
+
+mnist <- dataset_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
+x_train <- array_reshape(x_train,c(nrow(x_train), 784)) / 255
+x_test <- array_reshape(x_test,c(nrow(x_ttest), 784)) / 255
+y_train <- to_categorical(y_train,10)
+y_test <- to_categorical(y_test,10)
+
+model <- keras_model_sequential()
+model %>% 
+  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 128, activation = 'relu') %>% 
+  layer_dense(10, activation = 'softmax')
+
+model %>% compile(loss='categorical_crossentropy',
+                  optimizer=optimizer_rmsprop(),
+                  metrics=c('accuracy')
+)
+
+model %>% fit(
+  x_train, y_train,
+  epochs=50, batch_size=128,
+  validation_split=0.2
+)
+
+model %>% evaluate(x_test,y_test)
+model %>% predict_classes(x_test)
